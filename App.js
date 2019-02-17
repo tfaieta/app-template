414f1b592f6fa19215cc5bfd4f32990f04bd75e7
Tony Faieta <tfaieta@gmail.com>
2019-02-16 23:53:26 -0500

    Added more content

--- App.js
+++ App.js
@@ -1,25 +1,69 @@
 import React from "react";
-import { Text } from "react-native";
+import { ScrollView, SafeAreaView } from "react-native";
 import styled from "styled-components";
 import Card from "./components/Card";
+import { NotificationIcon } from "./components/Icons";
+import Logo from "./components/Logo";
 
 export default class App extends React.Component {
   render() {
     return (
       <Container>
-        <TitleBar>
-          <Avatar source={require("./assets/avatar.jpg")} />
-          <Name>Tony Faieta</Name>
-          <Title>Maker of things, student at UF 🐊</Title>
-        </TitleBar>
-        <SubTitle>See Blog Posts</SubTitle>
-        <Card
-          title="Blog Post"
-          image={require("./assets/background2.jpg")}
-          caption="Caption Text"
-          logo={require("./assets/logo-react.png")}
-          subtitle="Subtitle Text"
-        />
+        <SafeAreaView>
+          <ScrollView style={{ height: "100%" }}>
+            <TitleBar>
+              <Avatar source={require("./assets/avatar.jpg")} />
+              <Name>Tony Faieta</Name>
+              <Title>Maker of things, student at UF 🐊</Title>
+              <NotificationIcon
+                style={{ position: "absolute", right: 20, top: 5 }}
+              />
+            </TitleBar>
+            <ScrollView
+              style={{
+                flexDirection: "row",
+                padding: 20,
+                paddingLeft: 12,
+                paddingTop: 30
+              }}
+              horizontal={true}
+            >
+              <Logo
+                image={require("./assets/logo-framerx.png")}
+                text="Framer X"
+              />
+              <Logo image={require("./assets/logo-figma.png")} text="Figma" />
+            </ScrollView>
+            <SubTitle>See Blog Posts</SubTitle>
+            <ScrollView
+              horizontal={true}
+              style={{ paddingBottom: 30 }}
+              showsHorizontalScrollIndicator={false}
+            >
+              <Card
+                title="Blog Post"
+                image={require("./assets/background2.jpg")}
+                caption="Caption Text"
+                logo={require("./assets/logo-react.png")}
+                subtitle="Subtitle Text"
+              />
+              <Card
+                title="Blog Post"
+                image={require("./assets/background2.jpg")}
+                caption="Caption Text"
+                logo={require("./assets/logo-react.png")}
+                subtitle="Subtitle Text"
+              />
+              <Card
+                title="Blog Post"
+                image={require("./assets/background2.jpg")}
+                caption="Caption Text"
+                logo={require("./assets/logo-react.png")}
+                subtitle="Subtitle Text"
+              />
+            </ScrollView>
+          </ScrollView>
+        </SafeAreaView>
       </Container>
     );
   }
@@ -30,7 +74,7 @@ const SubTitle = styled.Text`
   font-weight: 600;
   font-size: 15px;
   margin-left: 15px;
-  margin-top: 50px;
+  margin-top: 20px;
   text-transform: uppercase;
 `;
 
